c35500a37e8f703ad67e9790537b68720df108f4
sekny <seknyim@gmail.com>
2023-04-20 13:30:19 +0700

    Init

---
+++ MyWidget/MyWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  MyWidgetBundle.swift
+//  MyWidget
+//
+//  Created by YIM SEKNY on 20/4/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MyWidget()
+//        MyWidgetLiveActivity()
+    }
+}
